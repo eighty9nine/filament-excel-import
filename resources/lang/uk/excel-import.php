403269b19767cd9aed1b73ef993b5bea09f8fb79
Sviat <s.fartushnyi@gmail.com>
2025-01-27 10:19:28 +0200

    Add Ukrainian translations for Excel import validation and sample download

---
+++ resources/lang/uk/excel-import.php
@@ -0,0 +1,7 @@
+<?php
+
+// translations for EightyNine/ExcelImportAction
+return [
+    'validation_failed' => 'Рядок :row не пройшов перевірку даних. Повідомлення: :messages',
+    'download_sample_excel_file' => 'Завантажити зразок файлу Excel',
+];
